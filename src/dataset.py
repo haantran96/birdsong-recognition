281016e14f6b84c41ae2056b898c168f6454accb
haantran96 <haantran96@gmail.com>
2020-06-23 05:25:52 +0300

    adding frequency mask for augmentations

--- src/dataset.py
+++ src/dataset.py
@@ -4,14 +4,14 @@ import pandas as pd
 import joblib
 from PIL import Image
 import librosa
-from utils import build_spectogram
-import audiomentations
+from utils import *
+#import audiomentations
 
 
 BASE_DIR = "./input/train_audio/"
 
 class BirdDatasetTrain:
-    def __init__(self, folds, mean, std):
+    def __init__(self, folds,freq_mask=False, crop = 512):
         df = pd.read_csv("./input/train_folds.csv")
 
         df = df[["filename","ebird_code", "ebird_lbl", "duration", "kfold"]]
@@ -19,20 +19,26 @@ class BirdDatasetTrain:
         self.filenames = df.filename.values
         self.ebird_lbls = df.ebird_lbl.values
         self.ebird_codes = df.ebird_code.values
-        self.durations = df.duration.values
-        self.aug = None   
-
+        self.freq_mask = freq_mask
+        self.crop = crop 
+    
     def __len__(self):
         return (len(self.filenames))
 
     
     def __getitem__ (self,item):
         fp = BASE_DIR + self.ebird_codes[item] + "/" +self.filenames[item]
-        duration = self.durations[item]
-        mel_spec = build_spectogram(fp,duration)
-        print (mel_spec)
+        mel_spec = build_spectogram(fp)
+        mel_spec = do_random_crop(mel_spec, self.crop)
+
+        mel_spec = (mel_spec - mel_spec.mean()) / (mel_spec.std()+1e-7)
+        
+        if self.freq_mask:
+            mel_spec = freq_mask(mel_spec)
+        
+        mel_spec = mel_spec.reshape([1,mel_spec.shape[0],mel_spec.shape[1]])
         return {
-            "audio": torch.tensor(mel_spec, dtype=torch.float),
+            "audio": torch.tensor(mel_spec,dtype=torch.float),
             "ebird_lbl":torch.tensor(self.ebird_lbls[item], dtype=torch.long)
         }
         
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,9 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import os
+import numpy as np 
+import pandas as pd 
 import librosa
 import joblib
 import glob 
-
+import random
 
 SAMPLE_RATE = 32000
 N_MELS = 128
@@ -14,12 +13,37 @@ FMIN = 20
 FMAX = SAMPLE_RATE//2
 
 
-def build_spectogram(path, duration):
-    y = None
-    if duration != 0:
-        y, _ = librosa.load(path, sr = SAMPLE_RATE, res_type="kaiser_fast", duration=duration)
+def freq_mask(spec, F=30, replace_with_zero=True):
+    num_mel_channels = spec.shape[0]
+    f = random.randrange(0, F)
+    f_zero = random.randrange(0, num_mel_channels - f)
+
+    # avoids randrange error if values are equal and range is empty
+    if (f_zero == f_zero + f): return spec
+
+    mask_end = random.randrange(f_zero, f_zero + f) 
+    if (replace_with_zero): 
+        spec[f_zero:mask_end] = 0
     else: 
-        y, _ = librosa.load(path, sr = SAMPLE_RATE, res_type="kaiser_fast" )
+        spec[f_zero:mask_end] = spec.mean()
+    return spec
+
+def do_random_crop(img,crop):
+    """https://github.com/OsciiArt/Freesound-Audio-Tagging-2019/blob/master/src/utils.py"""
+    img_new = np.zeros([img.shape[0], crop], np.float32)
+    if img.shape[1] < crop:
+        shift = np.random.randint(0, crop - img.shape[1])
+        img_new[:, shift:shift + img.shape[1]] = img
+    elif img.shape[1] == crop:
+        img_new = img
+    else:
+        shift = np.random.randint(0, img.shape[1] - crop)
+        img_new = img[:, shift:shift + crop]
+    return img_new
+
+
+def build_spectogram(path):
+    y, _ = librosa.load(path, sr = SAMPLE_RATE, res_type="kaiser_fast" )
     M = librosa.feature.melspectrogram(
             y,
             sr=SAMPLE_RATE,
